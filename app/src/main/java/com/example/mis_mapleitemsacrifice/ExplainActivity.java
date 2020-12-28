3ce0e757cea68a5c2f270953dc6a77488e01efbd
AnJungho <hotdog1993@naver.com>
2020-12-28 19:27:38 +0900

    frame layout

---
+++ app/src/main/java/com/example/mis_mapleitemsacrifice/ExplainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mis_mapleitemsacrifice;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class ExplainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_explain);
+
+        Button infoButton = (Button) findViewById(R.id.back_btn);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mis_mapleitemsacrifice/InventoryActivity.java
@@ -0,0 +1,26 @@
+package com.example.mis_mapleitemsacrifice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+
+        Button infoButton = (Button) findViewById(R.id.back_btn);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
